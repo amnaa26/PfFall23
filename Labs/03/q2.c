2846c833f95991c7b05b27e7e58fbe976624524c
Amna <142903458+amnaa26@users.noreply.github.com>
2023-09-12 10:57:40 +0500

    Update q2.c

--- Labs/03/q2.c
+++ Labs/03/q2.c
@@ -19,4 +19,26 @@ int main() {
             return 1;
 }// end main
                     
-           
+ question#2
+#include <stdio.h>
+
+int main() {
+           int n;
+           printf("Enter an integer: ");
+           scanf("%d",&n);
+
+            int i=0;
+            while(i<n) {
+               int r=0;
+                 while(r<n) {
+                      printf("*");
+                      r=r+1;
+                             };
+                     
+                      printf("\n");
+                      i=i+1;
+                          };
+            return 1;
+}// end main
+                    
+          
