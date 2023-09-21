e7d5fce342dad99b63fa1f2df4a2dd2fc76c4f9e
Amna <142903458+amnaa26@users.noreply.github.com>
2023-09-22 02:14:45 +0500

    Update Q6.c

--- Labs/05/Q6.c
+++ Labs/05/Q6.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+Write a C program to receive an 8-bit number into a variable and then check if its 4th and
+7th bits are on. If these bits are found to be on, then put them off.
+   */
+
 int main() 
 {
    int number, bit4, bit7;
