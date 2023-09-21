b02f23a0eae059efaa11ae0240dcf1791ea7e142
Amna <142903458+amnaa26@users.noreply.github.com>
2023-09-22 02:13:25 +0500

    Update Q8.c

--- Labs/05/Q8.c
+++ Labs/05/Q8.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+An android developer wants to design a mobile feature to control the brightness of the mobile phone according 
+to the surrounding light. In order to do it he uses an ambient light sensor (for the detection of surrounding light) 
+which is commonly built in in all latest android phones. It gives the value of light intensity in integers. 
+Write a C program for Light sensor value ranges from 0-1000, if it's exposed under sunshine (>500), 
+if it's evening then (0 ~ 100), lighting (100 to 500).
+   */
+
 int main()
 {
    int number;
