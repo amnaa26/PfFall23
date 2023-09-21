d96966dabc139bcf579c54dfa9c066a9ce0fd380
Amna <142903458+amnaa26@users.noreply.github.com>
2023-09-22 02:14:10 +0500

    Update Q7.c

--- Labs/05/Q7.c
+++ Labs/05/Q7.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+An online shopping store is providing discounts on the items due to the Eid. If the cost of items is more than 1999
+it will give a discount upto 50%. If the cost of shopping is 2000 to 4000, a 20% discount will be applied.
+If the cost of shopping is 4001 to 6000, a 30% discount will be applied. If it's more than 6000 then
+50% discount will be applied to the cost of shopping. Print the actual amount, saved amount and the amount after discount.
+   */
+
 int main()
 {
    double items_cost, shipping_cost, amount, amount_saved = 0;
