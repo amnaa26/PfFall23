6147dd112addd3b8246771374a1db2b364f5391d
Amna <142903458+amnaa26@users.noreply.github.com>
2023-10-10 19:41:22 +0500

    Add files via upload

---
+++ Labs/06/q9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int size, min, max;
+	int arr[10000];
+	printf("How many times do you want to take the input?:\n");
+	scanf("%d",&size);
+	for(int i = 0; i < size; ++i){
+		printf("Enter num %d: ", i+1);
+		scanf("%d",&arr[i]);
+		printf("\n");
+	}
+
+	max = min = arr[0];
+	for(int i = 1; i < size; ++i){
+		max = arr[i] > max ? arr[i] : max;
+		min = arr[i] < min ? arr[i] : min;
+	}
+	printf("\nMaximum value found is %d",max);
+	printf("\nMinimum value found is %d",min);
+}
