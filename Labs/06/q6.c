79a66f91144bee2567b8f9d546accb34f1f8987a
Amna <142903458+amnaa26@users.noreply.github.com>
2023-10-10 23:49:59 +0500

    Add files via upload

---
+++ Labs/06/q6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+	int userTurn, numOfMatches = 21;
+	printf("There are 21 matchsticks and you can pick 1, 2, 3 or 4 matchsticks\n");
+	printf("The one who has to pick the last matchstick loses.\n");
+	while(numOfMatches > 1) {
+		printf("Pick matchstick(s): ");
+		scanf("%d", &userTurn);
+		if (userTurn < 1 || userTurn > 4) {
+			printf("\nPick only 1, 2, 3 or 4 cards.\n");
+			continue;
+		}
+		
+		numOfMatches -= 5;		
+		printf("You picked %d matchstick(s)\n", userTurn);
+		printf("Computer picked %d matchstick(s)\n", 5 - userTurn);
+		printf("Remaining matchstick(s) are %d\n\n", numOfMatches);		
+	}
+
+	printf("HAHAHA You lost!\n");
+}
