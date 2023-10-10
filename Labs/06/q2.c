1a18c15409d13b8f968d0cc2a76e895da93f9a54
Amna <142903458+amnaa26@users.noreply.github.com>
2023-10-10 20:00:53 +0500

    Add files via upload

---
+++ Labs/06/q2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	int sum = 0;
+	for (int a = 0, b = 1; ;) {
+		if (a % 3 == 0 || a % 5 == 0 || a % 7 == 0)
+			sum += a;
+		if (b % 3 == 0 || b % 5 == 0 || b % 7 == 0)
+			sum += b;
+		if (a > 10000) break;
+		printf("%d ", a);
+		if (b > 10000) break;
+		printf("%d ", b);
+		a += b;
+		b += a;
+		
+	}
+	printf("\nThe sum of numbers divisble by 3, 5 or 7 is %d", sum);
+}
