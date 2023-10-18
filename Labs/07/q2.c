0d0462a9353152a08d2bac90a169e0fd3b6fff46
Amna <142903458+amnaa26@users.noreply.github.com>
2023-10-18 19:16:07 +0500

    Add files via upload

---
+++ Labs/07/q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int size, i, d;
+	int arr[4] = {6,1,5,4};
+	printf("Enter d:");
+	scanf("%d",&d);
+	printf("Enter the size of array:");
+	scanf("%d",&size);
+	for(i = 0; i < size; ++i){
+		printf("Enter num %d: ",i + 1);
+		scanf("%d", &arr[i]);
+	}
+	for(i = 0; i < d; ++i){
+		arr[size] = arr[i];
+		++size;
+	}
+	printf("[ ");
+	for(i = d; i < size; ++i){
+		arr[i - d] = arr[i];
+		printf("%d ", arr[i - d]);
+	}
+	printf("] ");
+}
