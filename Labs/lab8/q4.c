0c666b111603a4ea8d1720e727fb0d738ba7d3d7
Amna <142903458+amnaa26@users.noreply.github.com>
2023-10-25 20:41:45 +0500

    Update q4.c

--- Labs/lab8/q4.c
+++ Labs/lab8/q4.c
@@ -1,3 +1,8 @@
+/* Programmer: Amna(23K-0066)
+ * Desc: Validating password
+ * Date: 24/10/2023
+*/
+
 #include <stdio.h>
 #include <string.h>
 
@@ -6,13 +11,8 @@ int main() {
 	char pass[1000];
 	printf("Enter password:");
 	gets(pass);
-	if(strlen(pass) >= 8) {
-		if(strcmp(pass,check) == 0){
-			printf("Login successful. Welcome!");
-		}
-		else 
-			printf("Login failed. Incorrect password.");
-	}
+	if(strlen(pass) >= 8 && strcmp(check,pass) == 0) 
+		printf("Login successful. Welcome!");
 	else 
-			printf("Login failed. Incorrect password.");
+		printf("Login failed. In
 }
