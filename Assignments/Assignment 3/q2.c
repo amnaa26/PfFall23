6e3f23005cff50c35139dbabd35a733cbef840a1
Amna <142903458+amnaa26@users.noreply.github.com>
2023-12-10 16:40:09 +0500

    Update q2.c

---
+++ Assignments/Assignment 3/q2.c
@@ -0,0 +1,5 @@
+/*
+ * Programmer: Amna(23K-0066)
+ * Date: 10/12/23
+ * Desc: Creates a 2D array for multiple departments. Assigns each department with employees with random names, roles and stats. Finds the best department according to stats
+ */
