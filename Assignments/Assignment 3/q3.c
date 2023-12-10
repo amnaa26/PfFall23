822872a341532c26b2633300267ecfb63dc5d5f7
Amna <142903458+amnaa26@users.noreply.github.com>
2023-12-10 16:59:25 +0500

    Update q3.c

---
+++ Assignments/Assignment 3/q3.c
@@ -0,0 +1,8 @@
+/*
+*Programmer: Amna(23K-0066)
+*Date: 10/122023
+*Descrition: Create three text files named Department.txt, Personal.txt and Combine.txt. The Personal.txt file contains
+ ID and Name. Whereas the Department.txt file contains ID and Salary. Write a function that takes input
+ as record IDs and mergesthe details from both personal and department files and then add this entry into
+ the Combine.txt file (ID, Name, Salary).
+*/
