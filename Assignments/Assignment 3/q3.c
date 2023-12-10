eb7ec2c18a58cb64b9c051723f54b6e5d1d0e0d0
Amna <142903458+amnaa26@users.noreply.github.com>
2023-12-10 18:18:47 +0500

    Update q3.c

--- Assignments/Assignment 3/q3.c
+++ Assignments/Assignment 3/q3.c
@@ -6,3 +6,54 @@
  as record IDs and mergesthe details from both personal and department files and then add this entry into
  the Combine.txt file (ID, Name, Salary).
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 100
+#define f_personal "Personal.txt"
+#define f_department "Department.txt"
+#define f_combine "Combine.txt"
+
+int main() {
+    printf("Name: Amna\nID: 23K-0066\n\n");
+    char id[BUFFER_SIZE], line[BUFFER_SIZE], temp[BUFFER_SIZE];
+    
+    printf("Enter ID to merge: ");
+    fgets(id, BUFFER_SIZE, stdin);
+    id[strcspn(id, "\n")] = '\0';
+
+    FILE *personalFile = fopen(f_personal, "r");
+    FILE *departmentFile = fopen(f_department, "r");
+    FILE *combineFile = fopen(f_combine, "a");
+
+    if (personalFile == NULL || departmentFile == NULL || combineFile == NULL) {
+        perror("Error opening files");
+        return EXIT_FAILURE;
+    }
+
+    while (fgets(line, BUFFER_SIZE, personalFile) != NULL) {
+        if (strstr(line, id) != NULL) {
+            strcpy(temp, line);
+            break;
+        }
+    }
+
+    while (fgets(line, BUFFER_SIZE, departmentFile) != NULL) {
+        if (strstr(line, id) != NULL) {
+            strcat(temp, line);
+            break;
+        }
+    }
+
+    fprintf(combineFile, "%s\n", temp);
+
+    fclose(personalFile);
+    fclose(departmentFile);
+    fclose(combineFile);
+
+    printf("Merging operation completed.\n");
+    return EXIT_SUCCESS;
+}
+
