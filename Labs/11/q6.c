08601a7e02f7c546bebc1ce2b9facb13a2d39712
Amna <142903458+amnaa26@users.noreply.github.com>
2023-12-10 20:51:07 +0500

    Update q6.c

--- Labs/11/q6.c
+++ Labs/11/q6.c
@@ -1,10 +1,17 @@
+/*
+ * Programmer : Amna(23K-0066)
+ * Date :  26/11/2023
+ * Desc : Create a structure named Date having day, month and year as its elements. Store the current
+          date in the structure. Now add 45 days to the current date and display the final date.
+*/
+
 #include <stdio.h>
 
 // Define the structure for Date
 struct Date {
-    int day;
-    int month;
-    int year;
+    int d;
+    int m;
+    int y;
 };
 
 // Function to check if a year is a leap year
@@ -24,18 +31,18 @@ int getDaysInMonth(int month, int year) {
 // Function to add days to a date
 struct Date addDays(struct Date currentDate, int daysToAdd) {
     while (daysToAdd > 0) {
-        int daysInMonth = getDaysInMonth(currentDate.month, currentDate.year);
-        if (currentDate.day + daysToAdd > daysInMonth) {
-            daysToAdd -= (daysInMonth - currentDate.day + 1);
-            currentDate.day = 1;
-            if (currentDate.month == 12) {
-                currentDate.month = 1;
-                currentDate.year++;
+        int daysInMonth = getDaysInMonth(currentDate.m, currentDate.y);
+        if (currentDate.d + daysToAdd > daysInMonth) {
+            daysToAdd -= (daysInMonth - currentDate.d + 1);
+            currentDate.d = 1;
+            if (currentDate.m == 12) {
+                currentDate.m = 1;
+                currentDate.y++;
             } else {
-                currentDate.month++;
+                currentDate.m++;
             }
         } else {
-            currentDate.day += daysToAdd;
+            currentDate.d += daysToAdd;
             daysToAdd = 0;
         }
     }
@@ -46,14 +53,14 @@ int main() {
     // Get the current date
     struct Date currentDate;
     printf("Enter current date (day month year): ");
-    scanf("%d %d %d", &currentDate.day, &currentDate.month, &currentDate.year);
+    scanf("%d %d %d", &currentDate.d, &currentDate.m, &currentDate.y);
 
     // Add 45 days to the current date
     struct Date finalDate = addDays(currentDate, 45);
 
     // Display the final date
-    printf("Current Date: %02d/%02d/%04d\n", currentDate.day, currentDate.month, currentDate.year);
-    printf("Final Date: %02d/%02d/%04d\n", finalDate.day, finalDate.month, finalDate.year);
+    printf("Current Date: %02d/%02d/%04d\n", currentDate.d, currentDate.m, currentDate.y);
+    printf("Final Date: %02d/%02d/%04d\n", finalDate.d, finalDate.m, finalDate.y);
 
     return 0;
 }
